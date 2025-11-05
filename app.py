b07b7b6578af1bce672cd1f776de1aa6d236f4ee
JesusNolasco35 <24308060610037@cetis61.edu.mx>
2025-11-04 17:21:48 -0700

    iniciar secion, registro y base

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
 app = Flask(__name__)
+app.secret_key = 'c321234312312'  
+
+
 
 @app.route('/')
-def inicio():
-    return render_template('inicio.html')
+def index():
+     return render_template('index.html')
+
+@app.route('/login')
+def login():
+     return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+     return render_template('registro.html')
+
+@app.route('/logout')
+def logout():
+     session.clear()
+     flash('Has cerrado sesión correctamente.', 'info')
+     return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
